e0ba0be26daa2417b7a2f0ba31c5ea151ac58d69
bronlib <robert@brondijk.com>
2025-02-01 21:15:34 +0100

    Improved large_dist, now seems to work well

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ One way is shown in the batch file "large_dist.bat". In this proceeds in three s
 3. That local directory is copied to another, because during uninstall the installed directory will be removed
 4. The installation is uninstalled and the install directory is removed.
 
+Downside of this method is that the user's registry will be modified every time. This operation always has the possibility of failing and causing user's registry to be corrupted. 
+
+
+
--- large_dist.bat
+++ large_dist.bat
@@ -1,4 +1,6 @@
 
+@echo on
+
 set target_version=3.10.1
 
 set target_python=python-%target_version%.exe
@@ -7,7 +9,7 @@ set tmp_dir="%~dp0\Python_tmp"
 
 
 :: Define the URL and output file"
-set URL="https://www.python.org/ftp/python/%target_version%/%target_python%.exe"
+set URL="https://www.python.org/ftp/python/%target_version%/%target_python%"
 set OUTPUT=%target_python%
 
 :: Check if the file already exists
@@ -34,6 +36,8 @@ if exist %OUTPUT% (
 	goto end
 )
 
+goto end
+
 echo First: Install, unfortunately, Python wants to change the registry
 %target_python% /quiet InstallAllUsers=0  TargetDir=%tmp_dir% AssociateFiles=0 Shortcuts=0 Include_doc=0 Include_launcher=0 
 
@@ -43,8 +47,16 @@ xcopy %tmp_dir% %target_pythondir% /E /I /H
 echo Thrid: Uninstall to clean up the registry
 %target_python% /quiet /uninstall
 
-echo Finished
+
+del %OUTPUT%
+
+echo successful
+goto quit
+
 
 :end
+echo ERRORS, please check and retry
+goto quit
 
+:quit
 
