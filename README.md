3d0c28813969e3c2003df44cc397eb33c8316f58
bronlib <robert@brondijk.com>
2025-02-01 21:07:31 +0100

    large_dist.bat is defined, but must be tested further

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ImprovingPortablePythonForWindows
-imporving the installation of python for the PythonThemplate
+improving the installation of python for the PythonThemplate
+
+One point is how the Python distribution in Windows11 is downloaded. 
+It would be nicer if it went silently.
+
+One way is shown in the batch file "large_dist.bat". In this proceeds in three steps:
+1. The sought for python<version>.exe is downloaded
+2. it is installed to a local directory
+3. That local directory is copied to another, because during uninstall the installed directory will be removed
+4. The installation is uninstalled and the install directory is removed.
+
---
+++ large_dist.bat
@@ -0,0 +1,50 @@
+
+set target_version=3.10.1
+
+set target_python=python-%target_version%.exe
+set target_pythondir="%~dp0\Python%target_version%"
+set tmp_dir="%~dp0\Python_tmp"
+
+
+:: Define the URL and output file"
+set URL="https://www.python.org/ftp/python/%target_version%/%target_python%.exe"
+set OUTPUT=%target_python%
+
+:: Check if the file already exists
+if exist %OUTPUT% (
+    echo File already exists. Skipping download.
+    goto end
+)
+
+:: Check if the URL exists
+curl --head --silent --fail %URL% >nul 2>&1
+if errorlevel 1 (
+    echo URL does not exist or is unreachable.
+    goto end
+)
+
+:: Use curl to download the file
+curl -o %OUTPUT% %URL%
+
+:: Check if the download was successful
+if exist %OUTPUT% (
+    echo Download successful!
+) else (
+    echo Download failed!
+	goto end
+)
+
+echo First: Install, unfortunately, Python wants to change the registry
+%target_python% /quiet InstallAllUsers=0  TargetDir=%tmp_dir% AssociateFiles=0 Shortcuts=0 Include_doc=0 Include_launcher=0 
+
+echo Second: Copy 
+xcopy %tmp_dir% %target_pythondir% /E /I /H
+ 
+echo Thrid: Uninstall to clean up the registry
+%target_python% /quiet /uninstall
+
+echo Finished
+
+:end
+
+
