3685de6cd4be5a32df373478f9ee40bdde4a7aa1
bronlib <robert@brondijk.com>
2025-02-02 19:51:22 +0100

    created now a script that lists the name of directories. Now filterout the ones with the exe file

--- get_urls.bat
+++ get_urls.bat
@@ -1,39 +1,21 @@
-@echo on
+@echo off
 
-goto end
+
 :: Define the target URL
 set URL=https://www.python.org/ftp/python/
 
+
 :: Fetch the directory listing and save it to a file
-curl -s %URL% > temp.html
+curl  %URL% | findstr /B /r /c:"<a href=.[3,2]"  > temp.html
 
-:: Extract lines containing directory names
-findstr /B "<a href="  temp.html > raw_list.txt
+(for /f "tokens=2" %%a in (temp.html) do (echo %%a )) > filter.txt
+(for /f tokens^=^2^ Delims^=^"  %%a in (filter.txt) do (echo %%a) ) > filter2.txt
+(for /f tokens^=^1^ Delims^=/  %%a in (filter2.txt) do (echo %%a) ) > dirnames.txt
 
-:: Process the list to extract directory names
-for /f "tokens=2 delims=\"" %%A in (raw_list.txt) do (@echo %%A) > dir_list.txt
-
-
-@echo on
-REM setlocal enabledelayedexpansion
-
-set file=raw_list.txt
-
-for /f "tokens=*" %%A in (%file%) do (
-    set "line=%%A"
-    for /f "delims=" %%B in ('echo !line! ^| findstr /r /o "\"[^\"]*\""') do (
-        set "substring=%%B"
-        echo !substring!
-    )
-)
-
-REM endlocal
+del temp.html
+del filter.txt
+del filter2.txt
 
 
-
-:end
-
-for  /f "delims=<" %%A in (raw_list.txt) do (type %%A)
-
-
+:quit
 
