cc7e7d9cb3bb4a852f5220363a743bb00cb77702
bronlib <robert@brondijk.com>
2025-02-02 22:02:00 +0100

    Now only working for python 3....is 2 needed????

--- get_urls.bat
+++ get_urls.bat
@@ -6,7 +6,7 @@ set URL=https://www.python.org/ftp/python/
 
 
 :: Fetch the directory listing and save it to a file
-curl  %URL% | findstr /B /r /c:"<a href=.[3,2]"  > temp.html
+curl  %URL% | findstr /B /r /c:"<a href=.[3]"  > temp.html
 
 (for /f "tokens=2" %%a in (temp.html) do (echo %%a )) > filter.txt
 (for /f tokens^=^2^ Delims^=^"  %%a in (filter.txt) do (echo %%a) ) > filter2.txt
@@ -16,6 +16,12 @@ del temp.html
 del filter.txt
 del filter2.txt
 
+:ff
+
+(for /f %%a in (dirnames.txt) do (^
+curl --head --silent --fail %URL%/%%a/python-%%a.exe >nul 2>&1  
+if not errorlevel 1 echo %%a
+)) > validdirs.txt
+
 
 :quit
-
